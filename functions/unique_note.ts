c08ad1199ea6075960657e62a5df84834f303fd4
tkoyasak <72780548+tkoyasak@users.noreply.github.com>
2025-02-23 23:02:05 +0900

    refactor: improve unique_note function logic

--- functions/unique_note.ts
+++ functions/unique_note.ts
@@ -1,18 +1,22 @@
+import fs from 'node:fs'
 import { ulid } from 'ulid'
 
-const unique_note = async (tp: Tp, templates: string[]) => {
-  if ((await tp.file.content).length > 0) {
-    new tp.obsidian.Notice(
-      `ERROR: '${tp.config.target_file.path}' is not empty`,
-    )
+const unique_note = async (tp: Tp, dir: string) => {
+  if (!(tp.app.vault.adapter instanceof tp.obsidian.FileSystemAdapter)) {
     return
   }
 
-  const path = await tp.system.suggester(
+  const templates = fs
+    .readdirSync(tp.app.vault.adapter.getFullPath(dir), { withFileTypes: true })
+    .filter((dirent) => dirent.isFile())
+    .filter((dirent) => dirent.name !== 'unique-note.md')
+    .map((dirent) => dirent.name)
+
+  const target = await tp.system.suggester(
     templates,
     templates,
     true,
-    'Select a template',
+    "Select a template. If the file isn't empty, only the file name will be changed.",
   )
 
   const id = ulid()
@@ -21,10 +25,13 @@ const unique_note = async (tp: Tp, templates: string[]) => {
 
   tp.hooks.on_all_templates_executed(async () => {
     const file = tp.config.target_file
-    const template = tp.file.find_tfile(path)
-    // @ts-ignore
-    const content = await tp.app.vault.read(template)
-    await tp.app.vault.modify(file, content)
+    const template = tp.file.find_tfile(target)
+
+    if (template && (await tp.file.content).length === 0) {
+      const content = await tp.app.vault.read(template)
+      await tp.app.vault.modify(file, content)
+    }
+
     await tp.app.fileManager.processFrontMatter(file, (fm) => {
       fm.id = id
       fm.created = tp.file.creation_date('YYYY-MM-DD')
