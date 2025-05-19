08f59ed4f4d92f22d1be55e16e313a1901466fba
tkoyasak <72780548+tkoyasak@users.noreply.github.com>
2025-05-20 08:21:24 +0900

    refactor: build all in one go

--- projects/templater-functions/tsdown.config.ts
+++ projects/templater-functions/tsdown.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig } from 'tsdown'
+import { defineConfig, UserConfig } from 'tsdown'
 import modules from 'node:module'
 
-export default defineConfig(({ entry }) => ({
-  entry,
+const config: UserConfig = {
   outDir: 'dist',
   target: 'node20.18',
   format: 'cjs',
   minify: true,
-  external: [...modules.builtinModules],
-}))
+  external: [
+    ...modules.builtinModules,
+  ],
+} as const
+
+const entries = [
+  './functions/echo.ts',
+  './functions/ulid.ts',
+  './functions/unique_note.ts',
+] as const
+
+export default entries.map((entry) => defineConfig({ ...config, entry }))
